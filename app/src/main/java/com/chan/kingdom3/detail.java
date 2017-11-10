b0cc0c512f711859370b61ee46422d69ae700d9e
Simon Chan <619156737@qq.com>
2017-11-10 16:04:51 +0800

    very naive

--- app/src/main/java/com/chan/kingdom3/detail.java
+++ app/src/main/java/com/chan/kingdom3/detail.java
@@ -37,29 +37,21 @@ public class detail extends AppCompatActivity {
         Bundle extras = this.getIntent().getExtras();
         if(extras != null)
         {
-            data = extras.getString("goodsName");
+            data = extras.getString("Name");
             for(int i = 0; i < Informations.size(); i++)
             {
-                if(Informations.get(i).get("goodName").toString().equals(data))
+                if(Informations.get(i).get("Name").toString().equals(data))
                 {
                     item_NO = i;
                 }
             }
         }
 
-        final int[] imageID = {R.mipmap.enchatedforest, R.mipmap.arla, R.mipmap.devondale, R.mipmap.kindle,
-                R.mipmap.waitrose, R.mipmap.mcvitie, R.mipmap.ferrero, R.mipmap.maltesers, R.mipmap.lindt,
-                R.mipmap.borggreve};
-        ImageView goodsimage = (ImageView)findViewById(R.id.image_detail);
-        int resId = imageID[item_NO];
-        goodsimage.setImageResource(resId);
+        //设置图片
+        ImageView image = (ImageView)findViewById(R.id.image_detail);
+        image.setImageResource(Integer.parseInt(Informations.get(item_NO).get("image").toString()));
 
-        TextView PriceView = (TextView)findViewById(R.id.price_in_detail);
-        PriceView.setText(Informations.get(item_NO).get("Price").toString());
-
-        final TextView InfoView = (TextView)findViewById(R.id.info);
-        InfoView.setText(Informations.get(item_NO).get("infoType").toString()+" "+Informations.get(item_NO).get("info").toString());
-
+        //返回按钮
         ImageButton BackButton = (ImageButton)findViewById(R.id.back_button);
         BackButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,9 +60,11 @@ public class detail extends AppCompatActivity {
             }
         });
 
-        TextView goods_name = (TextView)findViewById(R.id.goods_name);
-        goods_name.setText(data);
+        //人物姓名
+        TextView character_name = (TextView)findViewById(R.id.character_name);
+        character_name.setText(data);
 
+        //星标按钮
         final ImageButton Star = (ImageButton)findViewById(R.id.Star);
         Star.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,37 +82,41 @@ public class detail extends AppCompatActivity {
             }
         });
 
-        ImageButton shoppingCart = (ImageButton)findViewById(R.id.shopping);
-        shoppingCart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                choiceName = Informations.get(item_NO).get("goodName").toString();
-                choicePrice = Informations.get(item_NO).get("Price").toString();
-                count++;
+//        ImageButton shoppingCart = (ImageButton)findViewById(R.id.shopping);
+//        shoppingCart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                choiceName = Informations.get(item_NO).get("goodName").toString();
+//                choicePrice = Informations.get(item_NO).get("Price").toString();
+//                count++;
+//
+//                Toast.makeText(detail.this,"商品已添加到购物车",Toast.LENGTH_SHORT).show();
+//
+//                Bundle add_goods_bundle = new Bundle();
+//                add_goods_bundle.putString("name", choiceName);
+//                add_goods_bundle.putInt("icon", imageID[item_NO]);
+//                Intent intentBroadcast = new Intent(DYNAMICACTION);
+//                intentBroadcast.putExtras(add_goods_bundle);
+//                sendBroadcast(intentBroadcast);
+//
+////                EventBus.getDefault().post(new MessageEvent(choiceName, choicePrice));
+//            }
+//        });
 
-                Toast.makeText(detail.this,"商品已添加到购物车",Toast.LENGTH_SHORT).show();
-
-                Bundle add_goods_bundle = new Bundle();
-                add_goods_bundle.putString("name", choiceName);
-                add_goods_bundle.putInt("icon", imageID[item_NO]);
-                Intent intentBroadcast = new Intent(DYNAMICACTION);
-                intentBroadcast.putExtras(add_goods_bundle);
-                sendBroadcast(intentBroadcast);
-
-//                EventBus.getDefault().post(new MessageEvent(choiceName, choicePrice));
-            }
-        });
-
-        List<Map<String,Object>> MoreList = new ArrayList<>();
-        String[] Message = {"一键下单","分享商品","不感兴趣","查看更多商品促销信息"};
-        for(int i = 0; i < 4; i++)
+        List<Map<String,Object>> one_info = new ArrayList<>();
+        String[] info_str = {Informations.get(item_NO).get("gender").toString(),
+                                Informations.get(item_NO).get("Kingdom").toString(),
+                                Informations.get(item_NO).get("birth").toString(),
+                                Informations.get(item_NO).get("death").toString(),
+                                Informations.get(item_NO).get("native_place").toString()};
+        for(int i = 0; i < 5; i++)
         {
             Map<String,Object> temp = new LinkedHashMap<>();
-            temp.put("message", Message[i]);
-            MoreList.add(temp);
+            temp.put("message", info_str[i]);
+            one_info.add(temp);
         }
-        ListView MoreListView = (ListView)findViewById(R.id.more);
-        SimpleAdapter moreListAdapter = new SimpleAdapter(this, MoreList,R.layout.more_list_layout,new String[]{"message"},new int[]{R.id.more_message});
+        ListView MoreListView = (ListView)findViewById(R.id.info_list);
+        SimpleAdapter moreListAdapter = new SimpleAdapter(this, one_info,R.layout.more_list_layout, new String[]{"message"},new int[]{R.id.more_message});
         MoreListView.setAdapter(moreListAdapter);
     }
 
@@ -129,21 +127,25 @@ public class detail extends AppCompatActivity {
 
     void initInformation()
     {
-        String[] goodName = new String[]{"Enchated Forest", "Arla Milk", "Devondale Milk", "Kindle Oasis", "waitrose 早餐麦片",
-                "Mcvitie's 饼干", "Ferrero Rocher","Maltesers","Lindt","Borggreve"};
-        String[] Price = new String[]{"¥ 5.00", "¥ 59.00", "¥ 79.00", "¥ 2399.00", "¥ 179.00",
-                "¥ 14.90", "¥ 132.59","¥ 141.43","¥ 139.43","¥ 28.90"};
-        String[] infoType = new String[]{"作者","产地","产地","版本","重量","产地","重量","重量","重量","重量"};
-        String[] info = new String[]{"Johanna Basford","德国","澳大利亚","8GB","2Kg","英国",
-                "300g","118g","249g","640g"};
+        int[] ImageID = {R.drawable.liubei,R.drawable.guanyu, R.drawable.zhangfei, R.drawable.zhugeliang, R.drawable.zhaoyun,
+                R.drawable.caochao, R.drawable.sunquan, R.drawable.simayi, R.drawable.wanglang, R.drawable.huangai};
+        String[] Name = getResources().getStringArray(R.array.charater_names);
+        String[] Kingdom = getResources().getStringArray(R.array.Kingdoms);
+        String[] gender = getResources().getStringArray(R.array.gender);
+        String[] birth = getResources().getStringArray(R.array.birth);
+        String[] death = getResources().getStringArray(R.array.death);
+        String[] native_place = getResources().getStringArray(R.array.native_place);
 
         for(int i = 0; i < 10; i++)
         {
             Map<String, Object> temp = new LinkedHashMap<>();
-            temp.put("goodName", goodName[i]);
-            temp.put("Price", Price[i]);
-            temp.put("infoType",infoType[i]);
-            temp.put("info", info[i]);
+            temp.put("Name", Name[i]);
+            temp.put("Kingdom", Kingdom[i]);
+            temp.put("gender",gender[i]);
+            temp.put("birth", birth[i]);
+            temp.put("death", death[i]);
+            temp.put("native_place", native_place[i]);
+            temp.put("image", ImageID[i]);
             Informations.add(temp);
         }
     }
